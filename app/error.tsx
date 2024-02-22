9376c2ce43c763089cf3e83dfb8fb25b2d2c3a5f
somesome <wkddnjsdlr1@gmail.com>
2024-02-22 23:12:06 +0900

    chore: add error page

---
+++ app/error.tsx
@@ -0,0 +1,31 @@
+"use client"; // Error components must be Client Components
+
+import { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string };
+  reset: () => void;
+}) {
+  useEffect(() => {
+    // Log the error to an error reporting service
+    console.error(error);
+  }, [error]);
+
+  return (
+    <div className="flex items-center justify-center gap-2 flex-col">
+      <h2 className="text-lg font-semibold">Something went wrong!</h2>
+      <Button
+        onClick={
+          // Attempt to recover by trying to re-render the segment
+          () => reset()
+        }
+      >
+        Try again
+      </Button>
+    </div>
+  );
+}
